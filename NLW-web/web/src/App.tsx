263601cfd166856878ff78fce10b2d258c887812
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2021-10-19 23:00:43 -0300

    adicionando frontend

---
+++ NLW-web/web/src/App.tsx
@@ -0,0 +1,14 @@
+import styles from './App.module.scss';
+import { LoginBox } from './componentes/LoginBox';
+import { MessageList } from './componentes/MessageList';
+
+export function App() {
+  return (
+    
+    <main className={styles.contentWrapper}>
+      <MessageList />
+      <LoginBox />
+    </main>
+  )
+}
+
---
+++ NLW-web/web/src/componentes/MessageList/index.tsx
@@ -0,0 +1,54 @@
+import { api } from '../../services/api'
+
+import styles from './styles.module.scss';
+
+
+import logoImg from '../../assets/logo.svg'
+import { useEffect } from 'react';
+
+export function MessageList () {
+  useEffect(()=> {
+    api.get('messages/last3').then(response=>{
+      console.log(response.data);
+      
+    })
+  }, [])
+
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥 </p>
+           <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/GuilhermeFRocha.png" alt="Guilherme Freitas" />
+              </div>
+              <span>Guilherme Freitas</span>
+            </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+           <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/GuilhermeFRocha.png" alt="Guilherme Freitas" />
+              </div>
+              <span>Guilherme Freitas</span>
+            </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+           <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/GuilhermeFRocha.png" alt="Guilherme Freitas" />
+              </div>
+              <span>Guilherme Freitas</span>
+            </div>
+        </li>
+      </ul>
+    </div>
+  )
+}
